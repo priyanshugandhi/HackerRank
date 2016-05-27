aa4c7fc074335e1797bf38932dbd8143a68c79d3
Working Rom <dpriyeshvk@live.co.uk>
2016-05-27 12:49:29 +0530

    String Usage

---
+++ yourName.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+fName = raw_input()
+lName = raw_input()
+print ("Hello " + fName + " " + lName + "! You just delved into python.")
+
+#optimized:
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = [raw_input()]
+a.append(raw_input())
+print "Hello"+str(a[0])+" "+str(a[1])+"! You just delved into python."
